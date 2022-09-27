e62538cf80bdcd1801605ff81784a1131e6ae013
Matthias Kutz <kutz.matthias@gmail.com>
2022-09-27 21:10:40 +0000

    Add websocket to api

--- pastenv-api/src/index.ts
+++ pastenv-api/src/index.ts
@@ -1,15 +1,26 @@
-import express from "express";
-import path from "path";
-const app = express();
-const port = 3000; // default port to listen
+import { Client } from '@stomp/stompjs';
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send('This is the pastenv-api!');
-} );
+const client = new Client({
+    brokerURL: 'ws://localhost:15674/ws',
+    debug (str) {
+      console.info(str);
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+  });
 
-// start the express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+  client.onConnect = (frame) => {
+    console.log('Received ' + frame.body);
+  };
+
+  client.onStompError = (frame) => {
+    // Will be invoked in case of error encountered at Broker
+    // Bad login/passcode typically will cause an error
+    // Complaint brokers will set `message` header with a brief message. Body may contain details.
+    // Compliant brokers will terminate the connection after any error
+    console.log('Broker reported error: ' + frame.headers.message);
+    console.log('Additional details: ' + frame.body);
+  };
+
+  client.activate();
