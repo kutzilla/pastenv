d0f04f8961435309e36bea128d3ca623c187bf27
Matthias Kutz <kutz.matthias@gmail.com>
2022-09-28 21:47:16 +0000

    Update navigation bar and styling

--- pastenv-gui/src/components/NavigationBar.vue
+++ pastenv-gui/src/components/NavigationBar.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="navigation-bar">
-      <ul>
-        <li><a>Home</a></li>
-        <li class="right"><a>About</a></li>
+      <ul class="navigation-bar-list">
+        <li id="title" class="nav-bar-entry"><a href="/">pastenv.io</a></li>
+        <li class="nav-bar-entry"><a href="/use-cases">use cases</a></li>
+        <li class="nav-bar-entry right"><a href="/about">about</a></li>
       </ul>
     </div>
   </template>
@@ -14,15 +15,15 @@
   </script>
   
 <style>
-ul {
+  ul.navigation-bar-list {
     list-style-type: none;
     margin: 0;
     padding: 0;
     overflow: hidden;
     background-color: white;
   }
-  
-  li {
+
+  li.nav-bar-entry  {
     float: left;
   }
   
@@ -30,16 +31,27 @@ ul {
     float: right;
   }
 
-  li a {
+  li.nav-bar-entry a {
     display: block;
-    color: black;
+    color: lightgray;
     text-align: center;
     padding: 14px 16px;
     text-decoration: none;
   }
   
-  li a:hover {
-    color: white;
-    background-color: black;
+  li.nav-bar-entry a:hover {
+    color: black;
+    background-color: white;
   }
+
+  li#title.nav-bar-entry a {
+    color: black;
+    font-style: bold;
+  }
+
+  li#title.nav-bar-entry a:hover {
+    color: lightgray;
+    background: white;
+  }
+
   </style>
