dc1044ea4c28b9e892d61ec1c852f91fc66f0fc2
Matthias Kutz <kutz.matthias@gmail.com>
2022-09-28 21:47:39 +0000

    Add behaviour to clipboard

--- pastenv-gui/src/components/ClipBoard.vue
+++ pastenv-gui/src/components/ClipBoard.vue
@@ -2,11 +2,50 @@
   <div class="clip-board">
     <h1>Clipboard</h1>
     <p>Paste your stuff here.</p>
+    <ul class="entry-list">
+      <li class="entry" v-for="e in entrys" :key="e.text">{{e.text}}</li>
+    </ul>
+    <input class="new-entry-field" placeholder="Enter your stuff here." v-model="newEntry" v-on:keyup.enter="addEntry(newEntry)" />
   </div>
 </template>
 
+<style>
+ul.entry-list {
+    list-style-type: none;
+    padding-left: 1%;
+    padding-right: 1%;
+    overflow: hidden;
+}
+li.entry {
+  text-align: left;
+  margin-bottom: 1%;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  padding-left: 1%;
+  border: 1px;
+  border-style: solid;
+  background-color: lightgrey;
+}
+input.new-entry-field {
+  width: 97.5%;
+  height: 3em;
+  
+}
+</style>
 <script>
 export default {
   name: "ClipBoard",
+    data() {
+    return {
+      newEntry: '',
+      entrys: []
+    }
+  },
+  methods: {
+    addEntry(entry) {
+      this.entrys.push({text: entry});
+      this.newEntry = ''
+    }
+  }
 };
 </script>
