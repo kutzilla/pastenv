8625fc62416cfc27da44c5d55ecd79cbfe3cc724
Matthias Kutz <kutz.matthias@gmail.com>
2022-10-01 15:54:18 +0000

    Add management for websocket connections and functionality to send all received messages to new clients

--- pastenv-api/src/index.ts
+++ pastenv-api/src/index.ts
@@ -11,18 +11,39 @@ const wss = new WebSocketServer({
   server
 });
 
+const webSockets: WebSocket[] = [];
+
+const messages : string[] = [];
+
 wss.on('connection', ws => {
 
-  console.log('New client connected');
+  webSockets.push(ws);
+  console.log(`New client connected. Currently are ${webSockets.length} client(s) connected`);
+
+  console.log('Sending all messages to new client');
+  messages.forEach(message => {
+    ws.send(message);
+  });
+
 
   ws.on('message', data => {
-    console.log(`Received : ${data}`);
+    console.log(`Received : ${data} and send it to all clients`);
+    messages.push(data.toString());
 
-    ws.send('Hello from Server!')
+    webSockets.forEach(i => {
+      i.send(data);
+    });
   });
 
   ws.on('close', () => {
-    console.log(`Connection closed by client`);
+    console.log(`Connection closed by client. WebSocket is going to be removed`);
+    for (let i = 0; i <  webSockets.length; i++) {
+      if (ws === webSockets[i]) {
+        webSockets.splice(i, 1);
+      }
+    }
+    console.log(`WebSocket removed. ${webSockets.length} client(s) are left.`);
+
   });
 });
 
