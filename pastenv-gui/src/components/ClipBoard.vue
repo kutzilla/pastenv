b7383f71be88a5747ca0784a0860857c3c5917ad
Matthias Kutz <kutz.matthias@gmail.com>
2022-10-02 14:26:30 +0000

    Add functionality to connect to websocket from clipboard-component

--- pastenv-gui/src/components/ClipBoard.vue
+++ pastenv-gui/src/components/ClipBoard.vue
@@ -5,7 +5,7 @@
     <ul class="entry-list">
       <li class="entry" v-for="e in entrys" :key="e.text">{{e.text}}</li>
     </ul>
-    <input class="new-entry-field" placeholder="Enter your stuff here." v-model="newEntry" v-on:keyup.enter="addEntry(newEntry)" />
+    <input class="new-entry-field" placeholder="Enter your stuff here." v-model="newEntry" v-on:keyup.enter="addEntry(newEntry);sendMessage(newEntry)" />
   </div>
 </template>
 
@@ -37,6 +37,7 @@ export default {
   name: "ClipBoard",
     data() {
     return {
+      connection: null,
       newEntry: '',
       entrys: []
     }
@@ -45,7 +46,21 @@ export default {
     addEntry(entry) {
       this.entrys.push({text: entry});
       this.newEntry = ''
+    },
+    sendMessage: (message) => {
+      console.log(this.connection);
+      this.connection.send(message);
     }
-  }
+  },
+  created: () => {
+    console.log('Starting connection to pastenv-api');
+    this.connection = new WebSocket('ws://localhost:15674/pastenv');
+
+    this.connection.onopen = event => {
+      console.log(event);
+      console.log('Successfully connected to the pastenv api...');
+    };
+  },
+
 };
 </script>
